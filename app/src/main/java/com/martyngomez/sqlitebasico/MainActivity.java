c93d66885ae9109d25cd9988409fa5deb52c02a7
Martin Gomez <martyngomez22@gmail.com>
2018-02-21 20:17:00 -0300

    Mapeo

--- app/src/main/java/com/martyngomez/sqlitebasico/MainActivity.java
+++ app/src/main/java/com/martyngomez/sqlitebasico/MainActivity.java
@@ -2,12 +2,40 @@ package com.martyngomez.sqlitebasico;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    Button all, delete;
+    EditText texto;
+    ListView lista;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        all = (Button) findViewById(R.id.all);
+        delete = (Button) findViewById(R.id.delete);
+        texto = (EditText) findViewById(R.id.texto);
+        lista = (ListView) findViewById(R.id.lista);
+
+        all.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
+
+
     }
 }
