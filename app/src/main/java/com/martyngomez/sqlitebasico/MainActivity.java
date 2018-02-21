e9aa31cf9725bc083c25f28387628d238602e364
Martin Gomez <martyngomez22@gmail.com>
2018-02-21 20:30:23 -0300

    Creación de Productos

--- app/src/main/java/com/martyngomez/sqlitebasico/MainActivity.java
+++ app/src/main/java/com/martyngomez/sqlitebasico/MainActivity.java
@@ -7,10 +7,16 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     Button all, delete;
     EditText texto;
     ListView lista;
+    List <Producto> listaProductos;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,10 +38,22 @@ public class MainActivity extends AppCompatActivity {
         delete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+
             }
         });
 
+    }
+
+    public void crearProductos()
+    {
+        listaProductos.add(new Producto(1, "TV",15000));
+        listaProductos.add( new Producto(2, "Notebook",18000));
+        listaProductos.add( new Producto(3, "Heladera",10000));
+        listaProductos.add( new Producto(4, "Tablet",2000));
+        listaProductos.add( new Producto(5, "Celular",10000));
+        listaProductos.add( new Producto(6, "Chromecast",2000));
+        listaProductos.add( new Producto(7, "Microhondas",5000));
+        listaProductos.add( new Producto(8, "Horno",8000));
 
     }
 }
