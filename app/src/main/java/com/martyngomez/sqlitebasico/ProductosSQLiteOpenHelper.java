86df832652cced1db66e9f41f64b2ec34ccc1a86
Martin Gomez <martyngomez22@gmail.com>
2018-02-22 12:28:22 -0300

    Open Helper, creacion de tablas y update.

--- app/src/main/java/com/martyngomez/sqlitebasico/ProductosSQLiteOpenHelper.java
+++ app/src/main/java/com/martyngomez/sqlitebasico/ProductosSQLiteOpenHelper.java
@@ -3,6 +3,7 @@ package com.martyngomez.sqlitebasico;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 /**
  * Created by MTN on 22/02/2018.
@@ -12,22 +13,29 @@ public class ProductosSQLiteOpenHelper extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "productos.db";
     private static final int DATA_BASE_VERSION = 1;
 
-    public static final String TABLE_PRODUCTS = "productos";
-    public static final String COLUMN_ID = "id";
-    public static final String COLUMN_NAME = "nombre";
-    public static final String COLUMN_PRICE = "precio";
+    public static final String CREATE_TABLE =
+            "CREATE TABLE PRODUCTOS ( " +
+                    "ID INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    "NOMBRE TEXT," +
+                    "PRECIO INTEGER )";
 
-    public ProductosSQLiteOpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+    public static final String DROP_TABLE = "DROP TABLE IF EXIST PRODUCTOS";
+
+    public ProductosSQLiteOpenHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATA_BASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(CREATE_TABLE);
+        Log.i("onCreate","Tabla productos creada");
 
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+        sqLiteDatabase.execSQL( DROP_TABLE);
+        onCreate(sqLiteDatabase);
 
     }
 }
